3a8eca050a2c93d5b00c78e14ceac95404494761
sreejaece401 <121920378+sreejaece401@users.noreply.github.com>
2023-01-04 11:29:11 +0530

    Add files via upload

---
+++ expressions/prog 11.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+	#include<cmath>
+	using namespace std;
+	
+
+	int main() {
+	    int h;
+	    cin>>h;
+	    int vi = 0,a = 9.8;
+	    cout<<sqrt(vi * vi + (2*a*h));
+	}
+
