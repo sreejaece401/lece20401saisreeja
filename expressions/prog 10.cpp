6440ecc4beec8fd8b37ce3d5cf6039bb66f196d6
sreejaece401 <121920378+sreejaece401@users.noreply.github.com>
2023-01-04 11:26:13 +0530

    Add files via upload

---
+++ expressions/prog 10.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+	#include<cmath>
+	using namespace std;
+	
+
+	int main() {
+	    int r;
+	    cin>>r;
+	    cout<<"area "<<M_PI * (r*r)<<"\n";
+	    cout<<"volume "<<1.33 * M_PI * (r*r*r);
+	}
+
