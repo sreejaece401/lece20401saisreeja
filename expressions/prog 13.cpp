2239418e326bcd3d4d047256cf5aff3562b84cd8
sreejaece401 <121920378+sreejaece401@users.noreply.github.com>
2023-01-04 11:30:51 +0530

    Add files via upload

---
+++ expressions/prog 13.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+	using namespace std;
+	int main(){
+	    int s;
+	    cin>>s;
+	    int dd,hh,mm;
+	    dd = s/86400;
+	    s = s % 86400;
+	    hh = s/3600;
+	    s = s % 3600;
+	    mm = s/60;
+	    s = s % 60;
+	    printf("%.d:%.2d:%.2d:%.2d",dd,mm,hh,s);
+	}
+
